02d4aa645307f0ebb85a41a791e55fa3fd7b99c5
kimkillaqueen <kimkillaqueen@gmail.com>
2020-04-11 23:23:49 -0800

    added checkpoint system

---
+++ Assets/Scripts/checkpointSystem/checkpointManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkpointManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Transform> checkPoints;
+    private int lastVisitedCheckPointIndex = 0;
+    void Start()
+    {
+        checkPoints.AddRange(GetComponentsInChildren<Transform>());
+        checkPoints.Remove(transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Transform findCheckpoint() {
+        return checkPoints[lastVisitedCheckPointIndex];
+    }
+
+    public List<Transform> getCheckPoints() {
+        return checkPoints;
+    }
+
+    public void setLatestCheckpoint(Transform checkpoint) {
+        int newIndex = checkPoints.FindIndex(0, checkPoints.Count, x => x == checkpoint);
+        if(newIndex > lastVisitedCheckPointIndex) {
+            lastVisitedCheckPointIndex = newIndex;
+        }
+    }
+}
---
+++ Assets/Scripts/checkpointSystem/respawnPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawnPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject checkPointManager;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "runner") {
+            // FindObjectOfType<checkpointManager>().findCheckpoint();
+            other.gameObject.transform.position = FindObjectOfType<checkpointManager>().findCheckpoint().position;
+            Debug.Log("respawning");
+            other.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            // Transform respawnPoint = checkPointManager.findCheckpoint();
+        }
+    }
+}
